826d079cbde2d9d6b0aa83f5c5e40143718be777
LEONARDO SANTOS SILVA <th796899@TAHTO.CORP>
2025-07-29 17:26:21 -0300

    criando o repositório

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log();
+const myLibrary = [];
+
+function Book(title, author, numberOfPages, bookRead) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.bookRead = function () { !bookRead ? 'not read yet' : 'read' };
+  this.info = function () { `${title} by ${author}, ${numberOfPages} pages, ${this.bookRead()}.` };
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+  
+}
